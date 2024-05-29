7b56d933580b1b30a531f9fe9213a4d9213a6b90
ash herr <74975936+ashherr@users.noreply.github.com>
2024-05-28 22:28:21 -0700

    photo update

--- script.js
+++ script.js
@@ -3,33 +3,29 @@ const eventHeadings = document.querySelectorAll('.event-heading');
 
 const imageUrlsByHeading = {
   "Demos from event 1": [
-    "https://pbs.twimg.com/media/F1tXV5YaAAAKMdH?format=jpg&name=large"
+    "https://i.postimg.cc/QMdPQRzg/tiat1.png"
   ],
   "Demos from event 2": [
-    "https://pbs.twimg.com/media/F1tWiH0aAAIoO4t?format=jpg&name=4096x4096",
-    "https://pbs.twimg.com/media/F1tWjm1aYAcbXuM?format=jpg&name=4096x4096"
+    "https://i.postimg.cc/L5p3TPXd/IMG-6906.jpg",
+    "https://i.postimg.cc/ry1Jpfnq/IMG-6898.jpg"
   ],
   "Demos from event 3": [
-    "https://pbs.twimg.com/media/F1tSkOeaMAAZWoH?format=jpg&name=4096x4096",
-    "https://pbs.twimg.com/media/F1tSkOdaYAIqWPf?format=jpg&name=4096x4096",
-    "https://pbs.twimg.com/media/F1tSkOhakAIa3iF?format=jpg&name=4096x4096",
-    "https://pbs.twimg.com/media/F1tSkOhaYAA-TK_?format=jpg&name=4096x4096"
+    "https://i.postimg.cc/1txGmFDg/IMG-7331.jpg",
+    "https://i.postimg.cc/85hhjCbZ/IMG-7300.jpg",
+    "https://i.postimg.cc/D06LPYHs/IMG-7314.jpg"
   ],
   "Demos from event 4": [
-    "https://pbs.twimg.com/media/F2UXqzjacAAihRE?format=jpg&name=4096x4096",
-    "https://pbs.twimg.com/media/F2UXuuUaIAAxheJ?format=jpg&name=4096x4096",
-    "https://pbs.twimg.com/media/F2UXqzjbYAAupKZ?format=jpg&name=4096x4096",
-    "https://pbs.twimg.com/media/F2UXqzibkAAzBSu?format=jpg&name=4096x4096",
-    "https://pbs.twimg.com/media/F2UXut6acAAezFa?format=jpg&name=4096x4096"
+    "https://i.postimg.cc/jd9LYbH2/IMG-7706.jpg",
+    "https://i.postimg.cc/VNPv5WHc/IMG-7734.jpg",
+    "https://i.postimg.cc/Gmy9c5KQ/IMG-7720.jpg",
+    "https://i.postimg.cc/rpGm4RvR/IMG-7749.jpg"
   ],
   "Demos from event 5": [
-    "https://pbs.twimg.com/media/GB0K2DpW8AAAfCU?format=jpg&name=4096x4096"
+    "https://i.postimg.cc/50n27d9x/Full-Size-Render.jpg"
   ],
   "Demos from event 6": [
-    "https://pbs.twimg.com/media/GB0NkCpXUAA34Va?format=jpg&name=4096x4096",
-    "https://pbs.twimg.com/media/GB0M3zrXgAAIDI7?format=jpg&name=large",
-    "https://pbs.twimg.com/media/GB0Mwq-WUAAj441?format=jpg&name=4096x4096",
-    "https://pbs.twimg.com/media/GB0NTvpWYAEG1TX?format=jpg&name=4096x4096"
+    "https://i.postimg.cc/dQZ0RXPK/4e034a1e-f135-4161-b970-124cc462.jpg",
+    "https://i.postimg.cc/mrqstLr1/IMG-0520.jpg"
   ]
 };
 
