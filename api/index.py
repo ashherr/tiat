f31c7799595ccfd78fa120b6c02aed20c065b9c4
ashherr <74975936+ashherr@users.noreply.github.com>
2025-04-24 14:43:02 -0700

    reset

--- api/index.py
+++
@@ -1,9 +0,0 @@
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # Redirect to main page
-        self.send_response(302)
-        self.send_header('Location', '/')
-        self.end_headers()
-        return 
